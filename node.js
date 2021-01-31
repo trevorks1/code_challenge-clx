d8347742964e180c4c20d8a88f5425f2b7554288
Trevor Sansalone <trevorksansalone@gmail.com>
2021-01-31 16:47:44 -0600

    correcting spelling for return of DTC

--- node.js
+++ node.js
@@ -1,10 +1,10 @@
 // creating a for loop to loop through 1 - 100
 for (var i = 1; i < 101; i++) {
-  // giving every multiple of 3 and 5 CLXDTX
-  if (i % 15 == 0) console.log('CLXDTX');
+  // giving every multiple of 3 and 5 CLXDTC
+  if (i % 15 == 0) console.log('CLXDTC');
   // giving every multiple of 3 CLX
   else if (i % 3 == 0) console.log('CLX');
-  // giving every multiple of 5 DTX
-  else if (i % 5 == 0) console.log('DTX');
+  // giving every multiple of 5 DTC
+  else if (i % 5 == 0) console.log('DTC');
   else console.log(i);
 }
