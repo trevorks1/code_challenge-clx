af75a296030aa22d9e8eb63a5ce5c5412b3d2714
Trevor Sansalone <trevorksansalone@gmail.com>
2021-01-31 00:27:26 -0600

    initial commit

--- node.js
+++ node.js
@@ -1,6 +1,6 @@
 for (var i = 1; i < 101; i++) {
-  if (i % 15 == 0) console.log('FizzBuzz');
-  else if (i % 3 == 0) console.log('Fizz');
-  else if (i % 5 == 0) console.log('Buzz');
+  if (i % 15 == 0) console.log('CLXDTX');
+  else if (i % 3 == 0) console.log('CLX');
+  else if (i % 5 == 0) console.log('DTX');
   else console.log(i);
 }
